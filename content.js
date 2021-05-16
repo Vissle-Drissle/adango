275718e5da91f0ceb87d7dc7486a1ff85b558a41
Vissle Drissle <22083670+Vissle-Drissle@users.noreply.github.com>
2021-05-15 20:21:12 -0400

    Fixed wrong element removal

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ else {
   var element = [document.getElementsByTagName("td")[2], document.getElementsByClassName("logo_links")[0], document.getElementById("ad_wrapper"), document.getElementById("ad_placeholder_td"), document.getElementById("right"), document.getElementById("buyers_ad"), document.getElementById("rect_ad")]
   element.forEach(
     function(x) {
-      if (x && location.pathname != "/login") {x.remove()}
+      if (x &&  ["/login", "/signupdir"].includes(location.pathname) === false) {x.remove()}
     }
   )
   var limit = 0
