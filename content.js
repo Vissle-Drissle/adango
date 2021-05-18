537d8f834a1630a617036e32eb4a572680319878
Vissle Drissle <22083670+Vissle-Drissle@users.noreply.github.com>
2021-05-18 04:24:37 -0400

    Removed more elements

--- content.js
+++ content.js
@@ -1,11 +1,12 @@
 if (window.location.href.startsWith("http://")) {window.location.href = window.location.href.replace("http://", "https://")}
 else {
-  var element = [document.getElementsByTagName("td")[2], document.getElementsByClassName("logo_links")[0], document.getElementById("ad_wrapper"), document.getElementById("ad_placeholder_td"), document.getElementById("right"), document.getElementById("buyers_ad"), document.getElementById("rect_ad")]
+  var element = [document.getElementsByTagName("td")[2], document.getElementsByClassName("logo_links")[0], document.getElementById("ad_wrapper"), document.getElementById("ad_placeholder_td"), document.getElementById("right"), document.getElementById("buyers_ad"), document.getElementById("rect_ad"), document.getElementById("rc-widget-845cca"), document.getElementById("clogo")]
   element.forEach(
     function(x) {
       if (x &&  ["/login", "/signupdir"].includes(location.pathname) === false) {x.remove()}
     }
   )
+  if (document.getElementsByClassName("fpix_header")[0]) {document.getElementsByClassName("fpix_header")[0].remove()}
   var limit = 0
   document.querySelectorAll("div").forEach(
     function(x) {
