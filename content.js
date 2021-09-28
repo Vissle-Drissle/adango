a5e0e6aea92e7ce08e45fc7927c7299251335f97
Vissle Drissle <22083670+Vissle-Drissle@users.noreply.github.com>
2021-09-28 19:21:35 -0400

    Wrong element removal fix

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ else {
   var element = [document.getElementsByTagName("td")[2], document.getElementsByClassName("logo_links")[0], document.getElementById("ad_wrapper"), document.getElementById("ad_placeholder_td"), document.getElementById("right"), document.getElementById("buyers_ad"), document.getElementById("rect_ad"), document.getElementById("rc-widget-845cca"), document.getElementById("clogo")]
   element.forEach(
     function(x) {
-      if (x && x.getElementsByTagName("input")[0] == undefined) {x.remove()}
+      if (x && x.getElementsByTagName("input")[0] == undefined && location.pathname != "/deletegroup") {x.remove()}
     }
   )
   var limit = 0
